ea7c86213ce314d672aabf9926d537c8dd4514e7
benuhdrill <bgmach2023@fau.edu>
2024-10-26 16:50:35 -0400

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Ben Gmach on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
